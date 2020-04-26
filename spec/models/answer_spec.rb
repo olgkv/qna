caff67a0a30220c9c275f3a63c567ef259bb1b8f
Oleg Suchkov <2oleg.ru@gmail.com>
2020-04-26 21:46:30 +0300

    Add relationship specs between Question and Answer

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  describe 'relationships' do
+    it { should belong_to :question }
+  end
+
   describe 'validations' do
     it { should validate_presence_of :body }
   end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  descibe 'validations' do
+  describe 'relationships' do
+    it { should have_many(:answers).dependent(:destroy) }
+  end
+
+  describe 'validations' do
     it { should validate_presence_of :title }
     it { should validate_presence_of :body }
   end
