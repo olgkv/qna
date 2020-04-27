dd3d2cbd5e950d97bf944d70630bce0c3be0b416
Oleg Suchkov <2oleg.ru@gmail.com>
2020-04-27 21:00:23 +0300

    Add Question controller spec on get :index

---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe QuestionsController, type: :controller do
+  describe 'GET #index' do
+    it 'populates an array of all questions' do
+      questions = FactoryBot.create_list(:question, 3)
+
+      get :index
+
+      expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it 'renders index view' do
+      get :index
+
+      expect(response).to render_template :index
+    end
+  end
+end
