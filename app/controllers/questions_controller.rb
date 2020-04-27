8986bc004390e2a77d35e50d48d4cd6106ccda27
Oleg Suchkov <2oleg.ru@gmail.com>
2020-04-27 22:29:09 +0300

    Add Questions controller post specs

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -15,9 +15,17 @@ class QuestionsController < ApplicationController
   def edit
   end
 
+  def create
+    @question = Question.create(question_params)
+  end
+
   private
 
   def set_question
     @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:title, :body)
   end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -52,4 +52,22 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :edit
     end
   end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new question in the database' do
+        count = Question.count
+
+        post :create, params: { question: { title: '123', body: '123' } }
+        expect(Question.count).to eq count + 1
+      end
+
+      it 'redirects to show view'
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the question'
+      it 're-renders new view'
+    end
+  end
 end
