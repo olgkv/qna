8b2b081df2a79ce5edfd54ae1b006501d348b0fc
Oleg Suchkov <2oleg.ru@gmail.com>
2020-04-29 14:19:29 +0300

    Add feature spec 'sing in'

---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature 'User can sign in', "
+In order to ask questions
+As an unauthorized user
+I'd like to be able to sign in
+" do
+  given(:user) { User.create!(email: 'user@test.com', password: '123456789') }
+
+  background { visit new_user_session_path }
+
+  scenario 'Registered user tries to sign in' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_on 'Log in'
+
+    expect(page).to have_content 'Signed in successfully.'
+  end
+
+  scenario 'Unregisterd user tries to sing in' do
+    fill_in 'Email', with: 'wrong@test.com'
+    fill_in 'Password', with: '123456789'
+    click_on 'Log in'
+
+    expect(page).to have_content 'Invalid Email or password'
+  end
+end
