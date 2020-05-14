e1dc48d020b50cd8ab15bdd5d5a3db07d7ef0d12
Oleg Suchkov <2oleg.ru@gmail.com>
2020-05-14 17:01:57 +0300

    Add Answers#new controller spec

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -5,6 +5,20 @@ RSpec.describe AnswersController, type: :controller do
   let(:question) { create :question }
   let(:answer) { create :answer, question: question }
 
+  describe 'GET #new' do
+    before { login(user) }
+
+    before { get :new, params: { question_id: question } }
+
+    it 'assigns a new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
   describe 'POST #create' do
     before { login(user) }
 
