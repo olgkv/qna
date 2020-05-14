c9ba620babcd46970c3e60cdce9aab96da8e5d5e
Oleg Suchkov <2oleg.ru@gmail.com>
2020-05-14 17:50:52 +0300

    Add User#author_of? model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,19 @@ RSpec.describe User, type: :model do
     it { should have_many(:questions).dependent(:destroy) }
     it { should have_many(:answers).dependent(:destroy) }
   end
+
+  describe '#author_of?' do
+    let(:user) { create(:user) }
+    let(:user1) { create(:user) }
+    let(:question) { create(:question, author: user) }
+    let(:question1) { create(:question, author: user1) }
+
+    it 'user is an author' do
+      expect(user).to be_author_of(question)
+    end
+
+    it 'user is not an author' do
+      expect(user).not_to be_author_of(question1)
+    end
+  end
 end
