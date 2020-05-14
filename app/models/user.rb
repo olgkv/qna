935c032f06ed83fab587ab9126d666629d86bbff
Oleg Suchkov <2oleg.ru@gmail.com>
2020-05-14 12:22:56 +0300

    Set up relationship between User and Answer, add specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :questions, dependent: :destroy
+  has_many :answers, dependent: :destroy
 
   def author_of?(resource)
     resource.user_id == id
