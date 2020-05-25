ba186f6fa40ac307ee3d409c1755bc35b9ceb010
Oleg Suchkov <2oleg.ru@gmail.com>
2020-05-26 02:57:03 +0300

    Author can edit question via AJAX

---
+++ app/javascript/packs/question.js
@@ -0,0 +1,8 @@
+$(document).on('turbolinks:load', function() {
+    $('.edit-question-link').on('click', function(e) {
+        e.preventDefault();
+        $(this).hide();
+        var questionId = $(this).data('questionId');
+        $('form#edit-question-' + questionId).removeClass('hidden');
+    })
+});
