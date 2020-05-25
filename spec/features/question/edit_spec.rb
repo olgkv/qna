185a5ea9e672517ee674df72dd4fcf05cd013f4a
Oleg Suchkov <2oleg.ru@gmail.com>
2020-05-26 02:50:16 +0300

    Add Question edit feature spec

---
+++ spec/features/question/edit_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+feature 'User can edit own a question' do
+  given!(:user) { create(:user) }
+  given!(:question) { create(:question, author: user) }
+
+  scenario 'Unauthenticated user cannot edit a question' do
+    visit questions_path question
+    within find(id: "question-#{question.id}") do
+      expect(page).not_to have_link 'Edit'
+    end
+  end
+
+  describe 'Authenticated user' do
+    given!(:user1) { create(:user) }
+    given!(:question1) { create(:question, author: user1, body: 'user1 question') }
+
+    before { sign_in(user) }
+
+    scenario 'can edit an own question', js: true do
+      visit question_path(question)
+      within find("#question-#{question.id}") do
+        click_on 'Edit'
+        fill_in 'Title', with: 'New Question Title'
+        fill_in 'Body', with: 'New Question Body'
+        click_on 'Save'
+
+        expect(page).not_to have_content question.title
+        expect(page).not_to have_content question.body
+        expect(page).to have_content 'New Question Title'
+        expect(page).to have_content 'New Question Body'
+        expect(page).not_to have_selector 'textarea'
+      end
+    end
+
+    scenario 'cannot edit an own question with invalid attributes', js: true do
+      visit question_path(question)
+      within find("#question-#{question.id}") do
+        click_on 'Edit'
+        fill_in 'Body', with: ''
+        click_on 'Save'
+
+        expect(page).to have_content question.title
+        expect(page).to have_content question.body
+      end
+
+      within '.question-errors' do
+        expect(page).to have_content "Body can't be blank"
+      end
+    end
+
+    scenario 'cannot edit a question that is not his own', js: true do
+      visit question_path(question1)
+      within find("#question-#{question1.id}") do
+        expect(page).not_to have_link 'Edit'
+      end
+    end
+  end
+end
