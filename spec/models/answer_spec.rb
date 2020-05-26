e00a7dd13adaecf63add7a3be3747bd6454ce85e
Oleg Suchkov <2oleg.ru@gmail.com>
2020-05-26 16:21:05 +0300

    Update Answer model spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -9,4 +9,40 @@ RSpec.describe Answer, type: :model do
   describe 'validations' do
     it { should validate_presence_of :body }
   end
+
+  describe 'best answer validation if the answer is choosen as the best' do
+    let!(:answer) { create(:answer, best: true) }
+
+    it { should validate_uniqueness_of(:best).scoped_to(:question_id) }
+  end
+
+  describe 'best answer validation if the answer is not choosen as best answer' do
+    let!(:answer) { create(:answer, best: false) }
+
+    it { should_not validate_uniqueness_of(:best).scoped_to(:question_id) }
+  end
+
+  describe '#set_best' do
+    let!(:user) { create(:user) }
+    let!(:question) { create(:question, author: user) }
+    let!(:answer1) { create(:answer, best: true, question: question) }
+    let!(:answer2) { create(:answer, best: false, question: question) }
+    let!(:answer3) { create(:answer, best: false, question: question) }
+
+    before do
+      answer2.set_best
+      answer1.reload
+      answer2.reload
+      answer3.reload
+    end
+
+    it 'set best answer' do
+      expect(answer2).to be_best
+    end
+
+    it 'does not set best answer to others answers' do
+      expect(answer1).not_to be_best
+      expect(answer3).not_to be_best
+    end
+  end
 end
