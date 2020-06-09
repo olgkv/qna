cd8a8f560dc241e0c000bd2876a18b3cb8b857d0
Oleg Suchkov <2oleg.ru@gmail.com>
2020-06-09 23:16:23 +0300

    User can add files to answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,8 @@ class Answer < ApplicationRecord
   belongs_to :question
   belongs_to :author, class_name: 'User', foreign_key: :user_id
 
+  has_many_attached :files
+
   validates :body, presence: true
 
   validates :best, uniqueness: { scope: :question_id }, if: :best?
--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -29,6 +29,16 @@ feature 'User can create an answer to the question', "
 
       expect(page).to have_content "Body can't be blank"
     end
+
+    scenario 'can add files to answer', js: true do
+      fill_in 'Body', with: 'answer body'
+
+      attach_file 'File', [Rails.root.join('spec/rails_helper.rb'), Rails.root.join('spec/spec_helper.rb')]
+      click_on 'Answer'
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
+    end
   end
 
   describe 'Unauthenticated user' do
